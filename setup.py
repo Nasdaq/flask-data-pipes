892904866ea934713d3a4807281ab0a25d14428f
scarbeau <jscarbeau@gmail.com>
2018-12-06 13:45:41 -0500

    package config

--- setup.py
+++ setup.py
@@ -4,22 +4,23 @@ Flask-Data-Pipes
 
 Simple, performant data pipelines
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='Flask-Data-Pipes',
-    version='1.0a',
-    url='',
-    license='',
+    version='1.0.3a',
+    url='https://github.com/Nasdaq/flask-data-pipes',
+    license='APACHE-2.0',
     author='Jonathon Scarbeau',
     author_email='jonathon.scarbeau@nasdaq.com',
     description='Simple, performant data pipelines',
     long_description=__doc__,
-    packages=['flask_data_pipes'],
+    packages=find_packages(exclude=['examples', ]),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
+    python_requires='>=3.6',
     install_requires=[
         'Flask',
         'Flask-SQLAlchemy',
